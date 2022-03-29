2786acb3ffd0aeb942d00524a96dbd6a9483ee74
Calvin Sun <hainan.sun@gmail.com>
2022-03-29 20:20:27 +0800

    Remove key

--- src/main/java/io/javabrains/DataStaxAstraProperties.java
+++ src/main/java/io/javabrains/DataStaxAstraProperties.java
@@ -1,13 +1,19 @@
 package io.javabrains;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 
 @ConfigurationProperties(prefix = "datastax.astra")
 public class DataStaxAstraProperties {
     private File securityConnectBundle;
+
+    public DataStaxAstraProperties() {
+    }
+
+    public DataStaxAstraProperties(File securityConnectBundle) {
+        this.securityConnectBundle = securityConnectBundle;
+    }
 
     public File getSecurityConnectBundle() {
         return securityConnectBundle;
